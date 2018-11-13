90dca34ede441df33c8665b6de53a9c0319d5a7e
BrianDX <boris.zaitsev@devexpress.com>
2018-11-13 14:20:15 +0300

    first commit

---
+++ CS/DashboardNextPrevTab/ViewerForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DashboardNextPrevTab {
+    partial class ViewerForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.Utils.Animation.Transition transition1 = new DevExpress.Utils.Animation.Transition();
+            DevExpress.Utils.Animation.SlideFadeTransition slideFadeTransition1 = new DevExpress.Utils.Animation.SlideFadeTransition();
+            this.dashboardViewer = new DevExpress.DashboardWin.DashboardViewer(this.components);
+            this.imageCollection = new DevExpress.Utils.SvgImageCollection(this.components);
+            this.transitionManager = new DevExpress.Utils.Animation.TransitionManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardViewer
+            // 
+            this.dashboardViewer.AllowMaximizeDashboardItems = false;
+            this.dashboardViewer.AllowPrintDashboard = false;
+            this.dashboardViewer.Appearance.BackColor = System.Drawing.SystemColors.Control;
+            this.dashboardViewer.Appearance.Options.UseBackColor = true;
+            this.dashboardViewer.DashboardSource = new System.Uri("Dashboards\\SalesDashboard.xml", System.UriKind.Relative);
+            this.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardViewer.Location = new System.Drawing.Point(0, 0);
+            this.dashboardViewer.Name = "dashboardViewer";
+            this.dashboardViewer.Size = new System.Drawing.Size(1016, 564);
+            this.dashboardViewer.TabIndex = 0;
+            // 
+            // imageCollection
+            // 
+            this.imageCollection.Add("Next", "image://svgimages/arrows/next.svg");
+            this.imageCollection.Add("Prev", "image://svgimages/arrows/prev.svg");
+            this.imageCollection.Add("Slideshow", "image://svgimages/icon builder/business_presentation.svg");
+            this.imageCollection.Add("ShowCaption", "image://svgimages/dashboards/showcaption.svg");
+            // 
+            // transitionManager
+            // 
+            this.transitionManager.ShowWaitingIndicator = false;
+            transition1.BarWaitingIndicatorProperties.Caption = "";
+            transition1.BarWaitingIndicatorProperties.Description = "";
+            transition1.Control = this.dashboardViewer;
+            transition1.LineWaitingIndicatorProperties.AnimationElementCount = 5;
+            transition1.LineWaitingIndicatorProperties.Caption = "";
+            transition1.LineWaitingIndicatorProperties.Description = "";
+            transition1.RingWaitingIndicatorProperties.AnimationElementCount = 5;
+            transition1.RingWaitingIndicatorProperties.Caption = "";
+            transition1.RingWaitingIndicatorProperties.Description = "";
+            slideFadeTransition1.Parameters.EffectOptions = DevExpress.Utils.Animation.PushEffectOptions.FromRight;
+            transition1.TransitionType = slideFadeTransition1;
+            transition1.WaitingIndicatorProperties.Caption = "";
+            transition1.WaitingIndicatorProperties.Description = "";
+            this.transitionManager.Transitions.Add(transition1);
+            // 
+            // ViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 564);
+            this.Controls.Add(this.dashboardViewer);
+            this.Name = "ViewerForm";
+            this.Text = "Dashboard Viewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.DashboardViewer dashboardViewer;
+        private DevExpress.Utils.SvgImageCollection imageCollection;
+        private DevExpress.Utils.Animation.TransitionManager transitionManager;
+    }
+}
+
---
+++ CS/DashboardNextPrevTab/ViewerForm.cs
@@ -0,0 +1,108 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWin;
+using DevExpress.XtraEditors;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DashboardNextPrevTab
+{
+    public partial class ViewerForm : XtraForm {
+        const int slideShowIntervalInMilliseconds = 3000;
+        Timer slideShowTimer = new Timer();
+        TabContainerDashboardItem TabContainer {
+            get {
+                Dashboard dashboard = dashboardViewer.Dashboard;
+                return dashboard.Items.SingleOrDefault(i => i is TabContainerDashboardItem) as TabContainerDashboardItem;
+            }
+        }
+        public ViewerForm() {
+            InitializeComponent();
+
+            dashboardViewer.CustomizeDashboardTitle += DashboardViewerCustomizeDashboardTitle;
+            dashboardViewer.SelectedTabPageChanged += DashboardViewerSelectedTabPageChanged;
+
+            SetUpTimer();
+        }
+        void SetUpTimer() {
+            slideShowTimer.Interval = slideShowIntervalInMilliseconds;
+            slideShowTimer.Tick += SlideShowTimerTick;
+        }
+        void DashboardViewerCustomizeDashboardTitle(object sender, CustomizeDashboardTitleEventArgs e) {
+            DashboardToolbarItem nextTabItem = new DashboardToolbarItem("Next tab", NextPrevTabItemClick);
+            nextTabItem.Tag = NextPrevValue.Next;
+            nextTabItem.SvgImage = imageCollection["Next"];
+            e.Items.Insert(0, nextTabItem);
+
+            DashboardToolbarItem prevTabItem = new DashboardToolbarItem("Previous tab", NextPrevTabItemClick);
+            prevTabItem.Tag = NextPrevValue.Prev;
+            prevTabItem.SvgImage = imageCollection["Prev"];
+            e.Items.Insert(0, prevTabItem);
+
+            DashboardToolbarItem showTabHederItem = new DashboardToolbarItem(TabContainer.ShowCaption, "Show tab headers", ShowHideTabHeadersItemClick);
+            showTabHederItem.SvgImage = imageCollection["ShowCaption"];
+            e.Items.Insert(0, showTabHederItem);
+
+            DashboardToolbarItem slideShowItem = new DashboardToolbarItem(slideShowTimer.Enabled, "Slideshow", SlideShowItemClick);
+            slideShowItem.SvgImage = imageCollection["Slideshow"];
+            e.Items.Insert(0, slideShowItem);
+        }
+        void SlideShowTimerTick(object sender, System.EventArgs e) {
+            transitionManager.StartTransition(dashboardViewer);
+            try {
+                ShowNextPrevTab(NextPrevValue.Next);
+            } finally {
+                transitionManager.EndTransition();
+            }
+        }
+        void ShowNextPrevTab(NextPrevValue value) {
+            TabContainerDashboardItem tabContainer = TabContainer;
+            if(tabContainer != null) {
+                int increment = value == NextPrevValue.Next ? 1 : -1;
+                string tabContainerName = tabContainer.ComponentName;
+                int selectedIndex = dashboardViewer.GetSelectedTabPageIndex(tabContainerName);
+                int pageCount = tabContainer.TabPages.Count;
+                dashboardViewer.SetSelectedTabPage(tabContainerName, (selectedIndex + pageCount + increment) % pageCount);
+            }
+        }
+        void NextPrevTabItemClick(DashboardToolbarItemClickEventArgs args) {
+            ShowNextPrevTab((NextPrevValue)args.Item.Tag);
+        }
+        void SlideShowItemClick(DashboardToolbarItemClickEventArgs args) {
+            if(args.Item.Checked.HasValue && args.Item.Checked.Value)
+                slideShowTimer.Start();
+            else
+                slideShowTimer.Stop();
+        }
+        void ShowHideTabHeadersItemClick(DashboardToolbarItemClickEventArgs args) {
+            TabContainerDashboardItem tabContainer = TabContainer;
+            if(tabContainer != null) {
+                if(args.Item.Checked.HasValue && args.Item.Checked.Value) {
+                    tabContainer.ShowCaption = true;
+                } else {
+                    tabContainer.ShowCaption = false;
+                }
+            }
+        }
+        void DashboardViewerSelectedTabPageChanged(object sender, SelectedTabPageChangedEventArgs e) {
+            TabContainerDashboardItem tabContainer = TabContainer;
+            if(tabContainer != null && !tabContainer.ShowCaption) {
+                string selectedTabPageName = e.Page;
+                dashboardViewer.Dashboard.Title.Text = TabContainer.TabPages[selectedTabPageName].Name;
+            }
+        }
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            if(disposing) {
+                slideShowTimer.Tick -= SlideShowTimerTick;
+                slideShowTimer.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+    enum NextPrevValue {
+        Next,
+        Prev
+    }
+}
