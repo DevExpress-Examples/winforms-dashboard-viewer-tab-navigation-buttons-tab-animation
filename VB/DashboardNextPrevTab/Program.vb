db5a71acf949dd90ab1b7d840fc17d14dc1fa862
DevExpressExampleBot <fakeEmail>
2021-12-08 15:19:08 +0300

    Source auto update [skip ci]

--- VB/DashboardNextPrevTab/Program.vb
+++ VB/DashboardNextPrevTab/Program.vb
@@ -1,21 +1,19 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 
 Namespace DashboardNextPrevTab
-	Friend NotInheritable Class Program
 
-		Private Sub New()
-		End Sub
+    Friend Module Program
 
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Shared Sub Main()
-			DevExpress.LookAndFeel.UserLookAndFeel.Default.SkinName = "The Bezier"
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New ViewerForm())
-		End Sub
-	End Class
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            DevExpress.LookAndFeel.UserLookAndFeel.Default.SkinName = "The Bezier"
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.Run(New ViewerForm())
+        End Sub
+    End Module
 End Namespace
